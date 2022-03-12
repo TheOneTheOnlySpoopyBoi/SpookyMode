35fd79da8776d2f76cb5d74ecb65642e62a6c13a
Spoop <minebloxgc@gmail.com>
2022-03-12 14:35:59 -0500

    Delete reset_health_on_player_death.lua
    Yes.

--- files/creep_skull/reset_health_on_player_death.lua
+++
@@ -1,8 +0,0 @@
-if GlobalsGetValue("SpookyMode_respawn_in_progress", "0") == "1" then
-	local entity_id = GetUpdatedEntityID()
-
-	local damage_model_component = EntityGetFirstComponentIncludingDisabled(entity_id, "DamageModelComponent")
-	local max_hp = ComponentGetValue2(damage_model_component, "max_hp")
-
-	ComponentSetValue2(damage_model_component, "hp", max_hp)
-end
