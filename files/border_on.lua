bdde9711296cbb71e9f8dfadb128578394bdd8d5
Spoop <minebloxgc@gmail.com>
2022-04-09 22:35:51 -0400

    Swanning stuff
    Swan tail, made sound closer, and moved its file

--- files/border_on.lua
+++ files/border_on.lua
@@ -2,5 +2,5 @@ local entity_id = GetUpdatedEntityID()
 local x, y = EntityGetTransform(entity_id)
 
 local owner = EntityGetRootEntity(entity_id)
-    GamePlaySound("mods/SpookyMode/files/audio/SpookyMode.bank", "swan_roar_wip", x+-70, y+100)
-    EntityLoad("mods/SpookyMode/files/lake_swan/lake_swan.xml", x+-70, y+300)
+    GamePlaySound("mods/SpookyMode/files/audio/SpookyMode.bank", "swan_roar_wip", x+-70, y+75)
+    EntityLoad("mods/SpookyMode/files/entities/animals/lake_swan/lake_swan.xml", x+-70, y+300)
